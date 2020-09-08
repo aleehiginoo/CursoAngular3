caa1940d214dba59c42b9ab5c9d1f7c62a8052ac
Alexandre Higino <alehigino@yahoo.com.br>
2020-09-08 20:17:32 -0300

    routerGuard - Inserindo protecao de rotas

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,9 +1,16 @@
 import { Class } from "@angular/core";
 
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Usuario } from "./acesso/usuario.model";
 import * as firebase from "firebase";
 
+@Injectable()
 export class Autenticacao {
+  public token_id: string;
+
+  constructor(private router: Router) {}
+
   public cadastrarUsuario(usuario: Usuario): Promise<any> {
     console.log("service", usuario);
 
@@ -29,7 +36,37 @@ export class Autenticacao {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, senha)
-      .then((resposta: any) => console.log(resposta))
+      .then((resposta: any) => {
+        firebase
+          .auth()
+          .currentUser.getIdToken()
+          .then((idToken: string) => {
+            this.token_id = idToken;
+            localStorage.setItem("idToken", idToken);
+            this.router.navigate(["/home"]);
+          });
+      })
       .catch((error: Error) => console.log(error));
+  }
+
+  public autenticado(): boolean {
+    if (this.token_id === undefined && localStorage.getItem("idToken")) {
+      this.token_id = localStorage.getItem("idToken");
+    }
+
+    if (this.token_id === undefined) this.router.navigate(["/"]);
+
+    return this.token_id !== undefined;
+  }
+
+  public sair(): void {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        localStorage.removeItem("idToken");
+        this.token_id = undefined;
+        this.router.navigate(["/"]);
+      });
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Autenticacao } from "../autenticacao.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  constructor(private autenticacao: Autenticacao) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public sair(): void {
+    this.autenticacao.sair();
   }
-
 }
